3165c7db4bb240592dda21067daa18aea7a612b7
Julio Campos <juliocesar5024@gmail.com>
2024-05-17 14:01:08 -0500

    integrate redux and useContext

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,30 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealsList/MealItem";
+import { useEffect, useLayoutEffect } from "react";
+import MealsList from "../components/MealsList/MealsList";
+
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  return <MealsList items={displayedMeals}/>
+  
+}
+
+export default MealsOverviewScreen;
+
+
